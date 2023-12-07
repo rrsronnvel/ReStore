753f61955eaf9c0cbcdc076cd3473db21f34e18e
rrsronnvel <alinas.ronnvel@gmail.com>
2023-12-07 18:04:19 +0800

    End of section 4

--- ReStore/client/src/index.tsx
+++ ReStore/client/src/index.tsx
@@ -6,10 +6,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { router } from './app/router/Routes.tsx';
+import { RouterProvider } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
