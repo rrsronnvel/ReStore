71b75bc4530ae35e19dd2f38f0b5d9524d2476a3
rrsronnvel <alinas.ronnvel@gmail.com>
2023-12-13 20:03:33 +0800

    End of section 5

--- ReStore/client/src/app/layout/App.tsx
+++ ReStore/client/src/app/layout/App.tsx
@@ -1,4 +1,3 @@
-import Catalog from "../../features/catalog/Catalog";
 import {
   Container,
   CssBaseline,
@@ -8,6 +7,8 @@ import {
 import Header from "./Header";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -27,6 +28,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
--- ReStore/client/src/features/catalog/ProductDetails.tsx
+++ ReStore/client/src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,19 @@
-import { Divider, Grid, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import axios from "axios";
+import {
+  Divider,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
+import NotFound from "../../app/error/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>();
@@ -10,16 +21,15 @@ export default function ProductDetails() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/products/${id}`)
-      .then((response) => setProduct(response.data))
+   id && agent.Catalog.details(parseInt(id))
+      .then((response) => setProduct(response))
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
   }, [id]);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <LoadingComponent message="Loading product..." />;
 
-  if (!product) return <h3>Product not found</h3>;
+  if (!product) return <NotFound />;
 
   return (
     <Grid container spacing={6}>
@@ -31,33 +41,37 @@ export default function ProductDetails() {
         />
       </Grid>
       <Grid item xs={6}>
-            <Typography variant="h3">{product.name} </Typography>
-            <Divider sx={{mb:2}} />
-            <Typography variant="h4" color='secondary'>${(product.price / 100).toFixed(2)} </Typography>
-            <TableContainer>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>{product.name} </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Description</TableCell>
-                        <TableCell>{product.description} </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Type</TableCell>
-                        <TableCell>{product.type} </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Brand</TableCell>
-                        <TableCell>{product.brand} </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Quantity in stock</TableCell>
-                        <TableCell>{product.quantityInStock} </TableCell>
-                    </TableRow>
-                </TableBody>
-            </TableContainer>
+        <Typography variant="h3">{product.name} </Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">
+          ${(product.price / 100).toFixed(2)}{" "}
+        </Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{product.name} </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>{product.description} </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>{product.type} </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Brand</TableCell>
+                <TableCell>{product.brand} </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Quantity in stock</TableCell>
+                <TableCell>{product.quantityInStock} </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </Grid>
   );
